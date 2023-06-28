70829af784d625dbfd40afe9b3bc5fe527e6a9eb
Erik Hougaard <erik@hougaard.com>
2023-06-28 08:38:00 -0700

    Update

--- S2S/S2S/obj/Debug/net6.0/S2S.AssemblyInfo.cs
+++ S2S/S2S/obj/Debug/net6.0/S2S.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("S2S")]
+[assembly: System.Reflection.AssemblyProductAttribute("BCCL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S2S")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
